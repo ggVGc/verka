5cf7201ddb6bbc317a03e18c892fa76b4a82ec98
Valter Sundström <valter.sundstrom@gmail.com>
2026-07-17 12:48:53 +0200

    build_all.sh

---
+++ build_all.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e
+
+pushd ./linka && cargo build --release && popd
+pushd ./driva && cargo build --release && popd
+pushd ./orka && cargo build --release && popd
+pushd ./nota && cargo build --release && popd
